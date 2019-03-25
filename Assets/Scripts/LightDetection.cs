7f131e45371c0c842c16355ad5bd7ff1c7c48681
mezzelo <35051000+Mezzelo@users.noreply.github.com>
2019-03-25 18:21:27 -0400

    asdfasdf
    asdfSDFASDFASDF

--- Assets/Scripts/LightDetection.cs
+++ Assets/Scripts/LightDetection.cs
@@ -11,7 +11,7 @@ public class LightDetection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(sunlight.forward);
     }
 
     // Update is called once per frame
