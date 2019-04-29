d756588c2f846c353527bb1fb4a41aeb07c43f26
mezzelo <35051000+Mezzelo@users.noreply.github.com>
2019-04-28 23:48:19 -0400

    GOGOGO

---
+++ Assets/Scripts/PortalCamScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamScript : MonoBehaviour
+{
+    public Transform mainCam;
+    public Transform portalSprite;
+    Vector3 mainCamRot;
+    Vector3 origCamRot;
+
+    // Start is called before the first frame update
+    void Start() {
+        mainCamRot = mainCam.rotation.eulerAngles;
+        origCamRot = transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.rotation = Quaternion.Euler(origCamRot + mainCam.rotation.eulerAngles - mainCamRot);
+        portalSprite.LookAt(transform.position);
+    }
+}
